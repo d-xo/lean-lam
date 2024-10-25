5a8fd43dd4659c8699b7994df01d887566564b7a
dxo <hi@d-xo.org>
2024-10-25 17:49:10 +0200

    flake: use lean from nixpkgs

--- flake.nix
+++ flake.nix
@@ -2,14 +2,13 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    lean4.url = "github:leanprover/lean4/v4.9.0";
   };
 
   outputs = ins : ins.flake-utils.lib.eachDefaultSystem (system : let
     pkgs = import ins.nixpkgs { inherit system; };
   in {
     devShells.default = pkgs.mkShell {
-      buildInputs = [ ins.lean4.defaultPackage.${system} ];
+      buildInputs = [ pkgs.lean4 ];
     };
   });
 }
