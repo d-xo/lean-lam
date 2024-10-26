6beb3db14f0bf82d9a6ef17b22075f79f1382be8
dxo <hi@d-xo.org>
2024-10-26 16:44:18 +0200

    flake: bump to lean 4.10

--- lakefile.lean
+++ lakefile.lean
@@ -7,9 +7,8 @@ package «lean-lam» where
 lean_lib «LeanLam» where
   -- add library configuration options here
 
-require batteries from git "https://github.com/leanprover-community/batteries" @ "v4.9.0"
-/- require auto from git "https://github.com/leanprover-community/lean-auto" -/
-require mathlib from git "https://github.com/leanprover-community/mathlib4" @ "v4.9.0"
+require batteries from git "https://github.com/leanprover-community/batteries" @ "v4.10.0"
+require mathlib from git "https://github.com/leanprover-community/mathlib4" @ "v4.10.0"
 
 
 @[default_target]
